6b6d72e07c4601c1f5af4008be8ab9112fd5e77a
David Kelly <kellydavid178@gmail.com>
2020-01-21 20:42:15 +0000

    Added fp intro and copied App2

--- src/main/scala/fpmax/Main.scala
+++ src/main/scala/fpmax/Main.scala
@@ -36,3 +36,58 @@ object App1 extends App {
 
   main
 }
+
+object Intro {
+    /**
+     * Functions are
+     * 
+     * 1. Total - For every input, they return an output.
+     * 2. Deterministic - For the same input, they return the same output.
+     * 3. Pure - The only effect of a function is in computing its return value.
+     * 
+     * These properties help us:
+     * 1. Reason about our programs using equational reasoning.
+     * 2. Refactor our programs without changing their meaning.
+     * 3. Test our programs more easily.
+     * 4. Invert control so caller always has control over callee.
+     * 5. Reason about our programs using type based reasoning.
+     */
+
+     def println(s: String): Unit = ???
+     def readLine(): String = ???
+     def parseInt(s: String): Int = ???
+}
+
+object App2 extends App {
+
+  def main: Unit = {
+
+    val name = readLine()
+
+    println("Hello " + name + ", welcome to the game!")
+
+    var exec = true
+
+    while (exec) {
+      val num = scala.util.Random.nextInt(5) + 1
+
+      println("Dear " + name + ", please guess a number between 1 and 5:")
+
+      // Partial Function:
+      val guess = readLine().toInt
+
+      if (guess == num) println("You guessed right, " + name + "!")
+      else println("You guessed wrong, " + name + ". The number was: " + num)
+
+      println("Do you want to continue, " + name + "?")
+
+      readLine() match {
+        case "y" => exec = true
+        case "n" => exec = false
+      }
+
+    }
+  }
+
+  main
+}
