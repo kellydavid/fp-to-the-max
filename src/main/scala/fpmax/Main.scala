67d03fe56c32662a026cf93bdc92c27138a6f756
David Kelly <kellydavid178@gmail.com>
2020-01-21 21:01:26 +0000

    Removed runtime errors

--- src/main/scala/fpmax/Main.scala
+++ src/main/scala/fpmax/Main.scala
@@ -38,27 +38,29 @@ object App1 extends App {
 }
 
 object Intro {
-    /**
-     * Functions are
-     * 
-     * 1. Total - For every input, they return an output.
-     * 2. Deterministic - For the same input, they return the same output.
-     * 3. Pure - The only effect of a function is in computing its return value.
-     * 
-     * These properties help us:
-     * 1. Reason about our programs using equational reasoning.
-     * 2. Refactor our programs without changing their meaning.
-     * 3. Test our programs more easily.
-     * 4. Invert control so caller always has control over callee.
-     * 5. Reason about our programs using type based reasoning.
-     */
 
-     def println(s: String): Unit = ???
-     def readLine(): String = ???
-     def parseInt(s: String): Int = ???
+  /**
+    * Functions are
+    *
+    * 1. Total - For every input, they return an output.
+    * 2. Deterministic - For the same input, they return the same output.
+    * 3. Pure - The only effect of a function is in computing its return value.
+    *
+    * These properties help us:
+    * 1. Reason about our programs using equational reasoning.
+    * 2. Refactor our programs without changing their meaning.
+    * 3. Test our programs more easily.
+    * 4. Invert control so caller always has control over callee.
+    * 5. Reason about our programs using type based reasoning.
+    */
+  def println(s: String): Unit = ???
+  def readLine(): String = ???
+  def parseInt(s: String): Int = ???
 }
 
 object App2 extends App {
+
+  def parseInt(s: String): Option[Int] = Try(s.toInt).toOption
 
   def main: Unit = {
 
@@ -74,16 +76,27 @@ object App2 extends App {
       println("Dear " + name + ", please guess a number between 1 and 5:")
 
       // Partial Function:
-      val guess = readLine().toInt
+      val guess = parseInt(readLine())
 
-      if (guess == num) println("You guessed right, " + name + "!")
-      else println("You guessed wrong, " + name + ". The number was: " + num)
+      guess match {
+        case None => println("You did not enter a number.")
+        case Some(guess) =>
+          if (guess == num) println("You guessed right, " + name + "!")
+          else
+            println("You guessed wrong, " + name + ". The number was: " + num)
+      }
 
-      println("Do you want to continue, " + name + "?")
+      var cont = true
 
-      readLine() match {
-        case "y" => exec = true
-        case "n" => exec = false
+      while (cont) {
+        cont = false
+        println("Do you want to continue, " + name + "?")
+
+        readLine().toLowerCase match {
+          case "y" => exec = true
+          case "n" => exec = false
+          case _   => cont = true
+        }
       }
 
     }
